1bf867a8ce8204fef0a73ee089e4c5bcf4f9ac32
Magdalena Sikorska <madzia.sikorska@gmail.com>
2012-04-17 12:41:11 +0200

    another fix

--- lib/omniauth/strategies/wordpress.rb
+++ lib/omniauth/strategies/wordpress.rb
@@ -11,6 +11,10 @@ module OmniAuth
         :authorize_url => '/oauth2/authorize',
         :token_url => '/oauth2/access_token'
       }
+      option :access_token_options, {
+        :header_format => 'OAuth %s',
+        :param_name => 'access_token'
+      }
 
       uid { raw_info['id'] }
 
@@ -28,10 +32,43 @@ module OmniAuth
         { 'raw_info' => raw_info }
       end
 
+      credentials do
+        {
+          'expires' => access_token.expires?,
+          'expires_at' => access_token.expires_at
+        }
+      end
+
+      def build_access_token
+        super.tap do |token|
+          token.options.merge!(access_token_options)
+        end
+      end
+
+      def access_token_options
+        options.access_token_options.inject({}) { |h,(k,v)| h[k.to_sym] = v; h }
+      end
+
       def raw_info
-        @raw_info ||= access_token.get("https://public-api.wordpress.com/me").parsed
+        @raw_info ||= access_token.get("/me").parsed
       rescue ::Errno::ETIMEDOUT
         raise ::Timeout::Error
+      end
+
+      def request_phase
+        options[:authorize_params] = client_params.merge(options[:authorize_params])
+        super
+      end
+
+      def auth_hash
+        OmniAuth::Utils.deep_merge(super, client_params.merge({
+          :grant_type => 'authorization_code'}))
+      end
+
+      private
+
+      def client_params
+        {:client_id => options[:client_id], :redirect_uri => callback_url ,:response_type => "code"}
       end
 
     end
