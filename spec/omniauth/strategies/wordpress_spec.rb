ac3131cde38d03232fa525f2cfa16bd4b88ab191
Magda Sikorska <madzia.sikorska@gmail.com>
2013-08-05 08:57:50 +0200

    upgrade oauth dependency; 0.2.0 version

--- spec/omniauth/strategies/wordpress_spec.rb
+++ spec/omniauth/strategies/wordpress_spec.rb
@@ -1,7 +1,10 @@
-require 'spec_helper'
 require 'omniauth-wordpress'
 
 describe OmniAuth::Strategies::Wordpress do
+  before(:all) do
+    OmniAuth.config.test_mode = true
+  end
+
   before :each do
     @request = double('Request')
     @request.stub(:params) { {} }
@@ -19,9 +22,12 @@ describe OmniAuth::Strategies::Wordpress do
     end
   end
 
-  it_should_behave_like 'an oauth2 strategy'
-
   describe '#client' do
+    it 'should be initialized with symbolized client_options' do
+      @options = { :client_options => { 'authorize_url' => 'https://example.com' } }
+      subject.client.options[:authorize_url].should == 'https://example.com'
+    end
+
     it 'has correct Wordpress site' do
       subject.client.site.should eq('https://public-api.wordpress.com')
     end
@@ -32,6 +38,34 @@ describe OmniAuth::Strategies::Wordpress do
 
     it 'has correct token url' do
       subject.client.options[:token_url].should eq('https://public-api.wordpress.com/oauth2/token')
+    end
+  end
+
+  describe '#authorize_params' do
+    it 'should include any authorize params passed in the :authorize_params option' do
+      @options = { :authorize_params => { :foo => 'bar', :baz => 'zip' } }
+      subject.authorize_params['foo'].should eq('bar')
+      subject.authorize_params['baz'].should eq('zip')
+    end
+
+    it 'should include top-level options that are marked as :authorize_options' do
+      @options = { :authorize_options => [:scope, :foo], :scope => 'bar', :foo => 'baz' }
+      subject.authorize_params['scope'].should eq('bar')
+      subject.authorize_params['foo'].should eq('baz')
+    end
+  end
+
+  describe '#token_params' do
+    it 'should include any authorize params passed in the :authorize_params option' do
+      @options = { :token_params => { :foo => 'bar', :baz => 'zip' } }
+      subject.token_params['foo'].should eq('bar')
+      subject.token_params['baz'].should eq('zip')
+    end
+
+    it 'should include top-level options that are marked as :authorize_options' do
+      @options = { :token_options => [:scope, :foo], :scope => 'bar', :foo => 'baz' }
+      subject.token_params['scope'].should eq('bar')
+      subject.token_params['foo'].should eq('baz')
     end
   end
 
